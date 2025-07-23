9f38549bfdf19b00649e02f40a964438e139c388
ls <larsschipper1311@gmail.com>
2025-07-23 14:26:07 +0200

    1
    cursus opdracht 4/6

---
+++ src/components/EventCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <SectionCard>
+    <template #header>
+      {{ title }}
+    </template>
+    {{ when }}
+    <template #footer>
+      <div class="space-y-4">
+        <p>{{ description }}</p>
+        <div class="flex justify-end">
+          <RoundButton @click="$emit('register', { title, when, description })">
+            Register
+          </RoundButton>
+        </div>
+      </div>
+    </template>
+  </SectionCard>
+</template>
+
+<script setup>
+import SectionCard from '@/components/SectionCard.vue'
+import RoundButton from '@/components/RoundButton.vue'
+
+defineProps({
+  title: String,
+  when: String,
+  description: String,
+})
+
+defineEmits(['register'])
+</script>
---
+++ src/components/RoundButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    type="button"
+    :class="[
+      'bg-gray-200 text-sm font-medium rounded-full px-3 py-1',
+      variants[variant] ?? variants.default,
+    ]"
+    @click.prevent="$emit('click')"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+const variants = {
+  default: 'border-gray-300 hover:bg-gray-100',
+  danger: 'bg-red-200 enabled:hover:bg-red-100 text-red-900',
+}
+
+defineProps({
+  variant: {
+    type: String,
+    default: 'default',
+  },
+})
+
+defineEmits(['click'])
+</script>
